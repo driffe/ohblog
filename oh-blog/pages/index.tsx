11056264ce265ca0d0c64f0777e6823f25043a84
SeyoungOh <33598679+driffe@users.noreply.github.com>
2024-01-27 14:12:49 +0000

    change theme and add text

--- oh-blog/pages/index.tsx
+++ oh-blog/pages/index.tsx
@@ -1,50 +1,72 @@
+import { Kalam } from 'next/font/google'
 
+const kalam = Kalam({
+  subsets: ["latin"],
+  weight: ["400", "700"], 
+});
 
 export default function Home() {
   return (
-    <div className="main">
-      {/* Title */}
-      <h1 className="text-area">Oh! Blog</h1>
+    <main className={`${kalam.className} back`}>
+      <div className="main">
+        {/* Title */}
+        <h1 className="text-area text-7xl mt-10">Oh! Blog</h1>
 
-      {/* Main Content Grid */}
-      <div className="menu-container">
-        {/* Profile */}
-        <div className="about-me border">
-          <h2 className="text-3xl font-bold text-black mb-2">About Me</h2>
-          {/* Profile content goes here */}
+        {/* Main Content Grid */}
+        <div className="menu-container">
+          {/* Profile */}
+          <div className="about-me boxes flex shadow-xl">
+            <div className='flex flex-col justify-between h-full'>
+              <h2 className="box-text">About Me</h2>
+              <p>I'm Seyoung Oh from South Korea, majoring in Software Engineering.</p>
+            </div>
+            {/* Profile content goes here */}
+          </div>
+
+          {/* Skills */}
+          <div className="skill boxes shadow-xl">
+            <div className='flex flex-col justify-between h-full'>
+                <h2 className="box-text">Skills</h2>
+                <p></p>
+            </div>            
+            {/* Skills content goes here */}
+          </div>
+
+          {/* Experiences */}
+          <div className="experience boxes shadow-xl">
+            <div className='flex flex-col justify-between h-full'>
+              <h2 className="box-text">Experiences</h2>
+              <p>Oh my experiences.</p>
+            </div>
+            {/* Experiences content goes here */}
+          </div>
+
+          {/* Projects */}
+          <div className="project boxes shadow-xl">
+            <div className='flex flex-col justify-between h-full'>
+              <h2 className="box-text">Projects</h2>
+              <p>Feel free to enter and review my projects.</p>
+            </div>
+            {/* Projects content goes here */}
+          </div>
+
+          {/* playground */}
+          <div className="playground boxes shadow-xl">
+            <div className='flex flex-col justify-between h-full'>
+              <h2 className="box-text">Playground</h2>
+              <p>Explore my areas of interest.</p>
+            </div>
+            {/* Projects content goes here */}
+          </div>
+
+          {/* Contacts */}
+          <div className="contact boxes shadow-xl">
+            <h2 className="box-text">Contact Me</h2>
+            {/* Contacts content goes here */}
+          </div>
+
         </div>
-
-        {/* Skills */}
-        <div className="skill border">
-          <h2 className="text-3xl font-bold text-black mb-2">Skills</h2>
-          {/* Skills content goes here */}
-        </div>
-
-        {/* Experiences */}
-        <div className="experience border">
-          <h2 className="text-3xl font-bold text-black mb-2">Work Experience</h2>
-          {/* Experiences content goes here */}
-        </div>
-
-        {/* Projects */}
-        <div className="project border">
-          <h2 className="text-3xl font-bold text-black mb-2">Projects</h2>
-          {/* Projects content goes here */}
-        </div>
-
-        {/* Projects */}
-        <div className="playground border">
-          <h2 className="text-3xl font-bold text-black mb-2">Projects</h2>
-          {/* Projects content goes here */}
-        </div>
-
-        {/* Contacts */}
-        <div className="contact border">
-          <h2 className="text-3xl font-bold text-black mb-2">Contact Me</h2>
-          {/* Contacts content goes here */}
-        </div>
-
       </div>
-    </div>
+    </main>
   );
 }
