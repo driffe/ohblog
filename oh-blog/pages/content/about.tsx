2945cc3034bf4688f1d91924fa9605cc9a90e3b1
SeyoungOh <33598679+driffe@users.noreply.github.com>
2024-02-16 14:50:28 +0000

    add responsive design

---
+++ oh-blog/pages/content/about.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+function About() {
+    return (
+        <div>
+            <h2>say</h2>
+        </div>
+    )
+};
+
+export default About;
--- oh-blog/pages/index.tsx
+++ oh-blog/pages/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Kalam } from 'next/font/google'
 
 const kalam = Kalam({
@@ -10,58 +12,70 @@ export default function Home() {
     <main className={`${kalam.className} back`}>
       <div className="main">
         {/* Title */}
-        <h1 className="text-area text-7xl mt-10">Oh! Blog</h1>
+        <h1 className="text-area text-7xl">Oh! Blog</h1>
 
         {/* Main Content Grid */}
         <div className="menu-container">
           {/* Profile */}
           <div className="about-me boxes flex shadow-xl">
-            <div className='flex flex-col justify-between h-full'>
-              <h2 className="box-text">About Me</h2>
-              <p>I'm Seyoung Oh from South Korea, majoring in Software Engineering.</p>
-            </div>
+            <a href='./content/about' className='h-full w-full'>
+              <div className='flex flex-col justify-between h-full'>
+                <h2 className="box-text">About Me</h2>
+                <p>I'm Seyoung Oh from South Korea, majoring in Software Engineering.</p>
+              </div>
+            </a>
             {/* Profile content goes here */}
           </div>
 
           {/* Skills */}
           <div className="skill boxes shadow-xl">
-            <div className='flex flex-col justify-between h-full'>
-                <h2 className="box-text">Skills</h2>
-                <p></p>
-            </div>            
+            <a href='' className='h-full w-full'>
+              <div className='flex flex-col justify-between h-full'>
+                  <h2 className="box-text">Skills</h2>
+                  <p></p>
+              </div>            
+            </a>
             {/* Skills content goes here */}
           </div>
 
           {/* Experiences */}
           <div className="experience boxes shadow-xl">
-            <div className='flex flex-col justify-between h-full'>
-              <h2 className="box-text">Experiences</h2>
-              <p>Oh my experiences.</p>
-            </div>
+            <a href='' className='h-full w-full'>
+              <div className='flex flex-col justify-between h-full'>
+                <h2 className="box-text">Experiences</h2>
+                <p>Oh my experiences.</p>
+              </div>
+            </a>
             {/* Experiences content goes here */}
           </div>
 
           {/* Projects */}
           <div className="project boxes shadow-xl">
-            <div className='flex flex-col justify-between h-full'>
-              <h2 className="box-text">Projects</h2>
-              <p>Feel free to enter and review my projects.</p>
-            </div>
+            <a href='' className='h-full w-full'>
+              <div className='flex flex-col justify-between h-full'>
+                <h2 className="box-text">Projects</h2>
+                <p>Feel free to enter and review my projects.</p>
+              </div>
+            </a>
             {/* Projects content goes here */}
           </div>
 
           {/* playground */}
           <div className="playground boxes shadow-xl">
-            <div className='flex flex-col justify-between h-full'>
-              <h2 className="box-text">Playground</h2>
-              <p>Explore my areas of interest.</p>
-            </div>
+            <a href='' className='h-full w-full'>
+              <div className='flex flex-col justify-between h-full'>
+                <h2 className="box-text">Playground</h2>
+                <p>Explore my areas of interest.</p>
+              </div>
+            </a>
             {/* Projects content goes here */}
           </div>
 
           {/* Contacts */}
           <div className="contact boxes shadow-xl">
-            <h2 className="box-text">Contact Me</h2>
+            <a href='' className='h-full w-full'>
+              <h2 className="box-text">Contact Me</h2>
+            </a>
             {/* Contacts content goes here */}
           </div>
 
